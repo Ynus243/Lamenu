d40a0c89ba0be26dd898ec5c08d78e7402f2fafb
Yunus <116478612+Ynus243@users.noreply.github.com>
2025-02-14 11:43:26 +0500

    Update index.vue

--- page/Home/index.vue
+++ page/Home/index.vue
@@ -1,18 +1,6 @@
 <script setup>
    import Header from '../../components/Header/index.vue' 
-import Intro from '../../components/Intro/index.vue'
-   import Order from '../../components/Order/index.vue'
-   import Yandex from '../../components/Yandex/index.vue'
- import Advice from '../../components/Advice/index.vue'
-   import Call from '../../components/Call/index.vue'
-  import Delivery from '../../components/Delivery/index.vue'
-  import Cafe from '../../components/Cafe/index.vue'
-   import Payment from '../../components/Payment/index.vue'
-   import Pos from '../../components/Pos/index.vue'
-  import Ways from '../../components/Ways/index.vue'
-//   import Price from '../../components/Price/index.vue'
-    import Registration from '../../components/Registration/index.vue'
-   import Footer from '../../components/Footer/index.vue'
+
 
 
 
@@ -21,19 +9,7 @@ import Intro from '../../components/Intro/index.vue'
 
 <template>
     <Header />
-    <Intro />
-    <Order />
-    <Yandex />
-    <Advice />
-    <Call />
-    <Delivery />
-    <Cafe />
-    <Payment />
-    <Pos />
-    <Ways />
-    <Price />
-    <Registration />
-    <Footer />
+    
 </template>
 
 <style></style>
